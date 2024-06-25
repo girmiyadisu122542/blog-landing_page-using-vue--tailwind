5d16cd9c3025bea89633412c91006d688b6267eb
Girmay Addisu <emmanda2050@gmail.com>
2024-06-25 16:06:16 +0300

    blog complete structure

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,176 @@
-<script setup>
-
-
-</script>
+<script setup></script>
 
 <template>
- 
+  <div class="h-screen bg-gray-100">
+    <header>
+      <div class="flex items-center justify-between bg-gray-900 p-5 text-white">
+        <div class="text-3xl">
+          <a href="">
+            <span class="text-red-700 font-bold text-center">Blog</span> Logo</a
+          >
+        </div>
+        <ul class="flex items-center justify-end gap-4">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </header>
+    <div
+      class="flex flex-col lg:flex-row items-center justify-between m-3 mx-auto"
+    >
+      <div class="text-xl">
+        <h1 class="text-2xl font-bold">It is Heading...</h1>
+        <p class="font-light">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+          perferendis numquam quibusdam eveniet repudiandae impedit quos, at
+          totam accusantium similique. Sit voluptate non debitis nisi sapiente
+          similique cupiditate ratione aspernatur ducimus libero alias
+          consequuntur officiis, consectetur mollitia eos dolor quod odio
+          placeat. Eius placeat ipsa soluta fugiat cupiditate rerum velit
+          temporibus tempore voluptas magni nihil, fugit pariatur officiis cum
+          maxime laudantium? Asperiores aliquid repellat libero soluta, corporis
+          repudiandae nostrum iste suscipit vitae. Odit amet nam vel odio fuga
+          omnis, voluptates, ratione, iure asperiores vero provident quos
+          reiciendis vitae aspernatur inventore sunt rerum sequi aliquam quaerat
+          est a non beatae repudiandae? Adipisci neque voluptates voluptatem
+          temporibus, ipsam, dicta dolorem alias facere natus similique quis
+          ratione reprehenderit omnis veritatis provident eligendi earum debitis
+          quisquam consequuntur magnam quos accusamus incidunt odio! Distinctio,
+          minus saepe explicabo accusamus id nostrum porro quidem, repellendus
+          delectus culpa, voluptatibus corrupti impedit cupiditate sit ad animi
+          aspernatur pariatur sint. Sequi consequatur inventore voluptas a optio
+          laboriosam quibusdam placeat fuga sunt maxime nulla odio nostrum at
+          omnis voluptatibus vitae debitis exercitationem, repudiandae facere!
+          Sunt, laborum alias commodi, deserunt nulla qui, cumque dolorum
+          cupiditate dignissimos eaque laudantium non veniam laboriosam. Iusto
+          culpa unde saepe at ut ratione eius animi placeat odit.
+        </p>
+      </div>
+      <img class="border rounded-lg mr-2" src="./images/01.png" alt="" />
+    </div>
+    <div
+      class="flex flex-wrap w-[90%] md:w-[100%] justify-start ml-0 mx-auto gap-[30px] m-3"
+    >
+      <div
+        class="md:w-[30%] p-4 w-full shadow-lg hover:transition-all ease-in-out delay-150 cursor-pointer duration-150"
+      >
+        <img class="rounded-md" src="./images/02.png" alt="" />
+        <h4 class="text-2xl font-bold text-center">Card Title</h4>
+        <p class="align-bottom">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          sunt accusamus, numquam error nulla sint distinctio dignissimos
+          laboriosam? Laboriosam sit quidem veniam sunt quas magnam
+          reprehenderit beatae corrupti vitae, quasi accusamus placeat, deserunt
+          amet voluptatibus. Praesentium aspernatur quis dolores vitae dolore
+          eius unde modi voluptate aliquid, inventore tempore, beatae aut!
+        </p>
+      </div>
+      <div class="md:w-[30%] p-4 w-full shadow-lg">
+        <img class="rounded-md" src="./images/03.png" alt="" />
+        <h4 class="text-2xl font-bold text-center">Card Title</h4>
+        <p class="align-bottom">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          sunt accusamus, numquam error nulla sint distinctio dignissimos
+          laboriosam? Laboriosam sit quidem veniam sunt quas magnam
+          reprehenderit beatae corrupti vitae, quasi accusamus placeat, deserunt
+          amet voluptatibus. Praesentium aspernatur quis dolores vitae dolore
+          eius unde modi voluptate aliquid, inventore tempore, beatae aut!
+        </p>
+      </div>
+      <div class="md:w-[30%] p-4 w-full shadow-lg">
+        <img class="rounded-md" src="./images/04.png" alt="" />
+        <h4 class="text-2xl font-bold text-center">Card Title</h4>
+        <p class="align-bottom">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          sunt accusamus, numquam error nulla sint distinctio dignissimos
+          laboriosam? Laboriosam sit quidem veniam sunt quas magnam
+          reprehenderit beatae corrupti vitae, quasi accusamus placeat, deserunt
+          amet voluptatibus. Praesentium aspernatur quis dolores vitae dolore
+          eius unde modi voluptate aliquid, inventore tempore, beatae aut!
+        </p>
+      </div>
+      <div class="md:w-[30%] p-4 w-full shadow-lg">
+        <img class="rounded-md" src="./images/05.png" alt="" />
+        <h4 class="text-2xl font-bold text-center">Card Title</h4>
+        <p class="align-bottom">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          sunt accusamus, numquam error nulla sint distinctio dignissimos
+          laboriosam? Laboriosam sit quidem veniam sunt quas magnam
+          reprehenderit beatae corrupti vitae, quasi accusamus placeat, deserunt
+          amet voluptatibus. Praesentium aspernatur quis dolores vitae dolore
+          eius unde modi voluptate aliquid, inventore tempore, beatae aut!
+        </p>
+      </div>
+      <div class="md:w-[30%] p-4 w-full shadow-lg">
+        <img class="rounded-md" src="./images/06.png" alt="" />
+        <h4 class="text-2xl font-bold text-center">Card Title</h4>
+        <p class="align-bottom">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          sunt accusamus, numquam error nulla sint distinctio dignissimos
+          laboriosam? Laboriosam sit quidem veniam sunt quas magnam
+          reprehenderit beatae corrupti vitae, quasi accusamus placeat, deserunt
+          amet voluptatibus. Praesentium aspernatur quis dolores vitae dolore
+          eius unde modi voluptate aliquid, inventore tempore, beatae aut!
+        </p>
+      </div>
+      <div class="md:w-[30%] p-4 w-full shadow-lg">
+        <img class="rounded-md" src="./images/07.png" alt="" />
+        <h4 class="text-2xl font-bold text-center">Card Title</h4>
+        <p class="align-bottom">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          sunt accusamus, numquam error nulla sint distinctio dignissimos
+          laboriosam? Laboriosam sit quidem veniam sunt quas magnam
+          reprehenderit beatae corrupti vitae, quasi accusamus placeat, deserunt
+          amet voluptatibus. Praesentium aspernatur quis dolores vitae dolore
+          eius unde modi voluptate aliquid, inventore tempore, beatae aut!
+        </p>
+      </div>
+      <div class="md:w-[30%] p-4 w-full shadow-lg">
+        <img class="rounded-md" src="./images/08.png" alt="" />
+        <h4 class="text-2xl font-bold text-center">Card Title</h4>
+        <p class="align-bottom">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          sunt accusamus, numquam error nulla sint distinctio dignissimos
+          laboriosam? Laboriosam sit quidem veniam sunt quas magnam
+          reprehenderit beatae corrupti vitae, quasi accusamus placeat, deserunt
+          amet voluptatibus. Praesentium aspernatur quis dolores vitae dolore
+          eius unde modi voluptate aliquid, inventore tempore, beatae aut!
+        </p>
+      </div>
+      <div class="md:w-[30%] p-4 w-full shadow-lg">
+        <img class="rounded-md" src="./images/09.png" alt="" />
+        <h4 class="text-2xl font-bold text-center">Card Title</h4>
+        <p class="align-bottom">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          sunt accusamus, numquam error nulla sint distinctio dignissimos
+          laboriosam? Laboriosam sit quidem veniam sunt quas magnam
+          reprehenderit beatae corrupti vitae, quasi accusamus placeat, deserunt
+          amet voluptatibus. Praesentium aspernatur quis dolores vitae dolore
+          eius unde modi voluptate aliquid, inventore tempore, beatae aut!
+        </p>
+      </div>
+    </div>
+    <footer
+      class="text-center fill-current bg-gray-900 text-white space-x-4 p-8"
+    >
+      <a
+        href="https://www.facebook.com"
+        class="rounded-full hover:translate-y-7 duration-300 hover:text-white"
+        target="_blank"
+        ><i class="fa-brands fa-facebook"></i
+      ></a>
+      <a href="https://www.instagram.com" target="_blank"
+        ><i class="fa-brands fa-instagram"></i
+      ></a>
+      <a href="https://www.x.com" target="_blank"
+        ><i class="fa-brands fa-twitter"></i
+      ></a>
+      <a href="https://www.whatsup.com" target="_blank"
+        ><i class="fa-brands fa-whatsapp"></i
+      ></a>
+      <p>&copy;2024 Girmay Addisu All right reserved</p>
+    </footer>
+  </div>
 </template>
